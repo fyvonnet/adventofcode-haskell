a87c8367d2784836d7bfc70a7696243c748e6beb
Franck Yvonnet <franck@yvonnet.fr>
2019-11-01 15:00:30 +0100

    Serial number is read from input file

--- 2018/day11.hs
+++ 2018/day11.hs
@@ -1,9 +1,5 @@
 import           AOC.Coord
-import           Control.Monad.Reader
-import           Data.Array
 import           Data.Ix
-import           Data.List
-import           Data.Maybe
 import qualified Data.Map as M
 
 
@@ -21,21 +17,22 @@ instance Eq Square where
 
 main :: IO ()
 main = do
-    let satSquare = foldl insertSquare M.empty $ range ((Coord 1 1), (Coord 300 300))
+    serialNum <- read <$> readFile "inputs/day11"
+    let satMatrix = foldl (insertSquare serialNum) M.empty $ range ((Coord 1 1), (Coord 300 300))
 
-    let (Square (Coord x1 y1) _ _) = foldl1 (findAll satSquare) $ squareCoords 3
+    let (Square (Coord x1 y1) _ _) = foldl1 (findAll satMatrix) $ squareCoords 3
     putStrLn $ show x1 ++ "," ++ show y1
 
-    let (Square (Coord x2 y2) s _) = foldl1 (findAll satSquare) [sq | s <- [1..300], sq <- squareCoords s]
+    let (Square (Coord x2 y2) s _) = foldl1 (findAll satMatrix) [sq | s <- [1..300], sq <- squareCoords s]
     putStrLn $ show x2 ++ "," ++ show y2  ++ "," ++ show s
 
 
 
-insertSquare :: SatMatrix -> Coord -> SatMatrix
-insertSquare sat c = M.insert c power sat
+insertSquare :: Int -> SatMatrix -> Coord -> SatMatrix
+insertSquare sn sat c = M.insert c power sat
     where
         (Coord x y) = c
-        power = (cellPower c) + (lu (Coord (x-1) y) sat) + (lu (Coord x (y-1)) sat) - (lu (Coord (x-1) (y-1)) sat)
+        power = (cellPower sn c) + (lu (Coord (x-1) y) sat) + (lu (Coord x (y-1)) sat) - (lu (Coord (x-1) (y-1)) sat)
 
 
 
@@ -50,23 +47,18 @@ squarePower sat (Coord x y) s =
     where l = s - 1
 
 
+
+lu :: Coord -> SatMatrix -> Int
 lu = M.findWithDefault 0
         
-    
 
 
-cellPower :: Coord -> Int
-cellPower (Coord x y) = do
-    let rackID = x + 10
-    (((rackID * y + serialNum) * rackID) `quot` 100 `mod` 10) - 5
+cellPower :: Int -> Coord -> Int
+cellPower sn (Coord x y) = (((rid * y + sn) * rid) `quot` 100 `mod` 10) - 5
+    where rid = x + 10
 
 
 
 squareCoords :: Int -> [Square]
 squareCoords size = map (\c -> Square c size 0) $ range ((Coord 1 1), (Coord l l))
     where l = (300 - size + 1)
-
-
-
-serialNum :: Int
-serialNum = 8141
