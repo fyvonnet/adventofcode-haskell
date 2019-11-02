8c6dc1c065a5e1c6de6bc8a528418062eb5a0bdc
Franck Yvonnet <franck@yvonnet.fr>
2019-11-02 10:57:26 +0100

    store SAT in array instead of map

--- 2018/day11.hs
+++ 2018/day11.hs
@@ -1,10 +1,12 @@
 import           AOC.Coord
-import           Data.Ix
+import           Data.Array
+import           Data.List
 import qualified Data.Map as M
 
 
-type SatMatrix = M.Map Coord Int
-data Square = Square Coord Int Int deriving Show
+type SatMap   = M.Map Coord Int
+type SatArray = Array Coord Int
+data Square   = Square Coord Int Int deriving Show
 
 instance Ord Square where
     compare (Square _ _ pa) (Square _ _ pb) = compare pa pb
@@ -14,44 +16,45 @@ instance Eq Square where
 
 
 
-
 main :: IO ()
 main = do
     serialNum <- read <$> readFile "inputs/day11"
-    let satMatrix = foldl (insertSquare serialNum) M.empty $ range ((Coord 1 1), (Coord 300 300))
 
-    let (Square (Coord x1 y1) _ _) = foldl1 (findAll satMatrix) $ squareCoords 3
+    let satMap    = foldl (insertSquare serialNum) M.empty $ range ((Coord 1 1), (Coord 300 300))
+    let zeroArray = listArray ((Coord 0 0), (Coord 300 300)) $ repeat 0
+    let satArray  = zeroArray // M.toList satMap
+
+    let (Square (Coord x1 y1) _ _) = foldl1 (findAll satArray) $ squareCoords 3
     putStrLn $ show x1 ++ "," ++ show y1
 
-    let (Square (Coord x2 y2) s _) = foldl1 (findAll satMatrix) [sq | s <- [1..300], sq <- squareCoords s]
+    let (Square (Coord x2 y2) s _) = foldl1 (findAll satArray) [sq | s <- [1..300], sq <- squareCoords s]
     putStrLn $ show x2 ++ "," ++ show y2  ++ "," ++ show s
 
 
 
-insertSquare :: Int -> SatMatrix -> Coord -> SatMatrix
+insertSquare :: Int -> SatMap -> Coord -> SatMap
 insertSquare sn sat c = M.insert c power sat
     where
         (Coord x y) = c
-        power = (cellPower sn c) + (lu (Coord (x-1) y) sat) + (lu (Coord x (y-1)) sat) - (lu (Coord (x-1) (y-1)) sat)
+        power =
+            (cellPower sn c) +
+            (M.findWithDefault 0 (Coord (x - 1)  y     ) sat) +
+            (M.findWithDefault 0 (Coord  x      (y - 1)) sat) -
+            (M.findWithDefault 0 (Coord (x - 1) (y - 1)) sat)
 
 
 
-findAll :: SatMatrix -> Square -> Square -> Square
-findAll sat sq (Square c s _) = max sq $ Square c s $ squarePower sat c s
+findAll :: SatArray -> Square -> Square -> Square
+findAll sat sq (Square c s _) = max sq $ Square c s power
+    where
+        (Coord x y) = c
+        power =
+            (sat ! (Coord (x     - 1) (y     - 1))) -
+            (sat ! (Coord (x + s - 1) (y     - 1))) -
+            (sat ! (Coord (x     - 1) (y + s - 1))) +
+            (sat ! (Coord (x + s - 1) (y + s - 1)))
 
-
-
-squarePower :: SatMatrix -> Coord -> Int -> Int
-squarePower sat (Coord x y) s =
-        (lu (Coord (x-1) (y-1)) sat) - (lu (Coord (x+l) (y-1)) sat) - (lu (Coord (x-1) (y+l)) sat) + (lu (Coord (x+l) (y+l)) sat)
-    where l = s - 1
-
-
-
-lu :: Coord -> SatMatrix -> Int
-lu = M.findWithDefault 0
         
-
 
 cellPower :: Int -> Coord -> Int
 cellPower sn (Coord x y) = (((rid * y + sn) * rid) `quot` 100 `mod` 10) - 5
