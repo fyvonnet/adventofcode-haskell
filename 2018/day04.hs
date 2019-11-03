dbba16674cc4209f264e51907d9b4755d357e745
Franck Yvonnet <franck@yvonnet.fr>
2019-11-03 14:53:24 +0100

    parse input with Megaparsec

--- 2018/day04.hs
+++ 2018/day04.hs
@@ -1,29 +1,31 @@
 
+import           Data.List
+import           Data.List.Split 
+import           Data.Time
+import           Data.Void
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
 import qualified Data.Map    as M
 import qualified Data.Vector as V
-import           Data.List
-import           Data.Time
-import           Data.Tuple
-import           Data.Maybe
-import           Text.Regex.Posix
 
 
 
 type Pair       = (Int, Int)
 type GuardData  = [(Int, [Pair])] -- ID and list of minute data (minute number and time slept)
-type Event      = (UniversalTime, Maybe Int)
 type MinuteVec  = V.Vector Int
 type GuardMap   = M.Map Int MinuteVec
+type Parser = Parsec Void String
+data Event = Wake | Sleep | Shift Int deriving (Show, Ord, Eq)
 
 
 
 main :: IO ()
 main = do
-    input <- (sort . map parseLine . lines) <$> readFile "inputs/day04"
+    input <- sort <$> (readFile "inputs/day04" >>= parseInput)
     let guardData = createGuardData input 0 V.empty M.empty
-    let run strat = print $ uncurry (*) $ snd $ foldl (applyStrat strat) (0, (0, 0)) guardData
-    run (\ms -> (sum $ map fst ms, snd $ maximum ms))
-    run maximum
+    let run strat = uncurry (*) $ snd $ foldl (applyStrat strat) (0, (0, 0)) guardData
+    print $ run (\ms -> (sum $ map fst ms, snd $ maximum ms))
+    print $ run maximum
 
 
 
@@ -33,26 +35,50 @@ applyStrat f current (gid, minutes) = max current (localMax, (gid, maxminute))
 
 
 
-createGuardData :: [Event] -> Int -> MinuteVec -> GuardMap -> GuardData
+createGuardData :: [(LocalTime, Event)] -> Int -> MinuteVec -> GuardMap -> GuardData
 
 createGuardData [] id v m =
     M.toList $ M.map (\v -> zip (V.toList v) [0..]) $ M.insert id v m
 
-createGuardData ((_, Just id'):es) id v m = do
-    let m' = if V.null v then m else M.insert id v m
-    let v' = M.findWithDefault (V.replicate 60 0) id' m'
-    createGuardData es id' v' m'
+createGuardData ((_, Shift id'):es) id v m = createGuardData es id' v' m'
+    where
+        m' = if V.null v then M.empty else M.insert id v m
+        v' = M.findWithDefault (V.replicate 60 0) id' m'
 
-createGuardData ((fa, Nothing):(wu, Nothing):es) id v m = do 
-    let minute = read . formatTime defaultTimeLocale "%M"
-    let v'     = V.accum (+) v [(m, 1) | m <- [(minute fa)..((minute wu) - 1)]]
-    createGuardData es id v' m
+createGuardData ((ts, Sleep):(tw, Wake):es) id v m = createGuardData es id v' m
+    where
+        getMin = todMin . localTimeOfDay
+        v'     = V.accum (+) v [(m, 1) | m <- [(getMin ts)..((getMin tw) - 1)]]
+
+createGuardData _ _ _ _ = error "something wrong with the events list"
 
 
 
-parseLine :: String -> Event
-parseLine s =
-    let dateTime = fromJust (parseTimeM True defaultTimeLocale "[%Y-%m-%d %H:%M]" (s =~ "\\[(.*)\\]"))
-        maybeGID = (read . tail) <$> (s =~~ "#[0-9]+")
-    in (dateTime, maybeGID)
+parseInput :: String -> IO [(LocalTime, Event)]
+parseInput raw = do
+    case parse (many inputLine) "" raw of
+        Left e  -> error $ errorBundlePretty e
+        Right x -> return x
 
+
+
+inputLine :: Parser (LocalTime, Event)
+inputLine = do
+    string "["
+    strTime <- many (noneOf "]")
+    string "] "
+    strEvt  <- many printChar
+    eol
+
+    let evt = parseEvent strEvt
+    let lt  = parseTimeOrError True defaultTimeLocale "%F %R" strTime
+    return (lt, evt)
+
+
+
+parseEvent :: String -> Event
+parseEvent str = 
+    case words str of
+        ("falls":_)         -> Sleep
+        ("wakes":_)         -> Wake
+        ("Guard":('#':n):_) -> Shift $ read n
