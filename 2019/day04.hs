dd4d5f6f04d6c4ec2ef05ae37eb7fc1b3dc08641
Franck Yvonnet <franck@yvonnet.fr>
2019-12-04 13:06:36 +0100

    filter ascending numbers before passing them to valid

--- 2019/day04.hs
+++ 2019/day04.hs
@@ -5,20 +5,16 @@ import Data.List.Split
 
 main :: IO ()
 main = do
-    input <- map read <$> splitOn "-" <$> readFile "inputs/day04"
-    print $ foldl' valid (0,0) [input !! 0 .. input !! 1]
+    input <- map (\s -> read s :: Int) <$> splitOn "-" <$> readFile "inputs/day04"
+    print $ foldl' valid (0,0) $ filter ascendant $ map show [input !! 0 .. input !! 1]
 
 
 
-valid :: (Int, Int) -> Int -> (Int, Int)
-valid (c1, c2) n = (c1', c2') where
-    lst   = show n
-    asc   = ascendant lst
+valid :: (Int, Int) -> [Char] -> (Int, Int)
+valid (c1, c2) lst = (c1', c2') where
     len2s = filter (>= 2) $ map length $ group lst
-    two   = not $ null len2s
-    stwo  = elem 2 len2s
-    c1'   = if (asc &&  two) then (c1 + 1) else c1
-    c2'   = if (asc && stwo) then (c2 + 1) else c2
+    c1'   = if (not $ null len2s) then (c1 + 1) else c1
+    c2'   = if (elem 2 len2s)     then (c2 + 1) else c2
 
 
 
