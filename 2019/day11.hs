bc05177e58b13fe08e2838529a1a0fac7c5468fa
Franck Yvonnet <franck@yvonnet.fr>
2019-12-11 08:58:53 +0100

    code cleaning

--- 2019/day11.hs
+++ 2019/day11.hs
@@ -6,41 +6,49 @@ import           Data.Map (Map)
 import qualified Data.Map as M
 
 
+type Hull = Map Coord Int
 data Direction = UP | RIGHT | DOWN | LEFT deriving (Show, Enum)
 data RobotState = RS 
     { ics   :: ICState
     , dir   :: Direction
     , coord :: Coord
-    , hull  :: Map Coord Int } deriving Show
+    , hull  :: Hull } deriving Show
 
 
 
 main :: IO ()
 main = do
     ics <- loadCode "inputs/day11"
-    let (output, ics') = runIntCode [0] ics
-    print $ M.size $ hull $ until donePainting step (RS ics UP (Coord 0 0) M.empty)
-    let h = hull $ until donePainting step (RS ics UP (Coord 0 0) (M.singleton (Coord 0 0) 1))
-    let k = M.keys h
-    putStrLn $ unlines $ map (map (setPixel h)) $ groupByLine $ sort $ range (head k, last k)
+    let [h1, h2] = map (paintHull ics) [M.empty, (M.singleton (Coord 0 0) 1)]
+    print $ M.size h1
+    let k = M.keys h2
+    putStrLn $ unlines $ map (map (displayPanel h2)) $ groupByLine $ sort $ range (head k, last k)
+
+
+paintHull :: ICState -> Hull -> Hull
+paintHull i h = hull $ until donePainting paintPanel (RS i UP (Coord 0 0) h)
+
 
 groupByLine :: [Coord] -> [[Coord]]
 groupByLine = groupBy (\(Coord _ y0) (Coord _ y1) -> y0 == y1)
 
-setPixel :: Map Coord Int -> Coord -> Char
-setPixel m c = if M.findWithDefault 0 c m == 0 then ' ' else '#'
 
-step :: RobotState -> RobotState
-step (RS ics dir coord hull) = (RS ics' dir' coord' hull') where
-    color = M.findWithDefault 0 coord hull
+displayPanel :: Hull -> Coord -> Char
+displayPanel m c = if M.findWithDefault 0 c m == 0 then ' ' else '#'
+
+
+paintPanel :: RobotState -> RobotState
+paintPanel (RS ics dir coord hull) = (RS ics' dir' coord' hull') where
     ([color', t], ics') = runIntCode [color] ics
-    dir' = turn t dir
+    color  = M.findWithDefault 0 coord hull
+    dir'   = turn t dir
     coord' = move dir' coord
-    hull' = M.insert coord color' hull
+    hull'  = M.insert coord color' hull
 
 
 donePainting :: RobotState -> Bool
 donePainting = not . isRunning . ics 
+
 
 turn :: Int -> Direction -> Direction
 turn 0 UP   = LEFT
