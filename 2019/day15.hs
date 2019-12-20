23352fe710b050ce10b56e5adabd78dded5d19b5
Franck Yvonnet <franck@yvonnet.fr>
2019-12-20 16:52:31 +0100

    merge the navigate and propagate functions

--- 2019/day15.hs
+++ 2019/day15.hs
@@ -1,49 +1,41 @@
-import           AOC.Coord
-import           Data.Either (fromLeft)
+import           Data.Either (fromLeft, fromRight)
 import           IntCode
 
-data Robot = RB { _trail :: [Int], _ics :: ICState } deriving (Show)
+data Robot = RB { _trail :: [Int], _ics :: ICState }
 
 
 main :: IO ()
 main = do
     ics <- loadCode "inputs/day15"
-    let robot = navigate [(RB [] ics)]
+    let robot = fromLeft undefined $ navigate 0 [(RB [] ics)]
     print $ length $ _trail robot
-    print $ propagate 0 [(RB [] (_ics robot))]
+    print $ fromRight undefined $ navigate 0 [(RB [] (_ics robot))]
 
 
-propagate :: Int -> [Robot] -> Int
-propagate tm [] = tm
-propagate tm (r:rs) = propagate tm' (rs ++ rbs) where
-    trail  = _trail r
-    direcs = if null trail then [1..4] else filter (/= (moveBack $ head trail)) [1..4]
-    tm' = max tm (length $ _trail r)
-    rbs = fromLeft undefined $ try [] r direcs
-
-
-navigate :: [Robot] -> Robot
-navigate (r:rs) = do
+navigate :: Int -> [Robot] -> Either Robot Int
+navigate tm [] = Right tm
+navigate tm (r:rs) = do
     let trail  = _trail r
-    let direcs = if null trail then [1..4] else filter (/= (moveBack $ head trail)) [1..4]
+    let direcs = if null trail then [1..4] else filter (/= (turnBack $ head trail)) [1..4]
+    let tm' = max tm (length $ _trail r)
     case try [] r direcs of
-        Left rbs -> navigate (rs ++ rbs)
-        Right r  -> r
+        Left rbs -> navigate tm' (rs ++ rbs)
+        Right r  -> Left r
 
 
 try :: [Robot] -> Robot -> [Int] -> Either [Robot] Robot
 try rbs _  []     = Left rbs
 try rbs rb (d:ds)
     | output == 0 = try rbs rb ds
-    | output == 1 = try (robot':rbs) rb ds
-    | output == 2 = Right robot' where
-    robot' = (RB (d:_trail rb) ics')
+    | output == 1 = try (r:rbs) rb ds
+    | output == 2 = Right r where
+    r = (RB (d:_trail rb) ics')
     ([output], ics') = runIntCode [d] (_ics rb)
 
 
-moveBack :: Int -> Int
-moveBack 1 = 2
-moveBack 2 = 1
-moveBack 3 = 4
-moveBack 4 = 3
+turnBack :: Int -> Int
+turnBack 1 = 2
+turnBack 2 = 1
+turnBack 3 = 4
+turnBack 4 = 3
 
