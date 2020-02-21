c9627fd4c9ad3dcc491cef41526d3db916d7f7d1
Franck Yvonnet <franck@yvonnet.fr>
2020-02-21 10:39:03 +0100

    code fiddling

--- 2019/day22.hs
+++ 2019/day22.hs
@@ -1,69 +1,52 @@
 import           AOC.Common
 import           Data.Foldable (foldl')
-import           Data.List     (find)
-import           Data.Maybe    (fromJust)
-import           Data.Tuple    (swap)
-import           Data.Vector   (Vector, (//))
+import           Data.List     (lookup, sort, zip)
+import           Data.Tuple    (snd, swap)
 import           Data.Void     (Void)
 import           Text.Megaparsec      (Parsec, errorBundlePretty, many, oneOf, parse, (<|>))
 import           Text.Megaparsec.Char (newline, string)
-import qualified Data.Vector as V
 
 
 type Parser = Parsec Void String
-data Technique = NEWSTACK | CUTCARDS Int | INCREMENT Int deriving Eq
+type Deck = [Int]
+type Shuffle = Deck -> Deck
+
 
 
 main :: IO ()
 main = do 
-    input <- readFile "inputs/day22" >>= parseInput
+    shuffle <- readFile "inputs/day22" >>= parseInput
 
-    let deck = foldl' applyTechnique [0..10006] input
-    print $ fst $ fromJust $ find ((== 2019) . snd) $ zip [0..] deck
+    let deck = shuffle [0..10006]
+
+    case lookup 2019 $ zip deck [0..] of
+        Nothing -> error "Not found!"
+        Just x  -> print x
    
-    --let deck2 = [0..119315717514046]
-    --print $ findRepeat $ iterate (\d -> foldl' applyTechnique d input) deck2
 
-
-applyTechnique :: [Int] -> Technique -> [Int]
-
-applyTechnique deck NEWSTACK = reverse deck
-
-applyTechnique deck (CUTCARDS n) = uncurry (++) $ swap $ splitAt n' deck
-    where n' = if n > 0 then n else (length deck) + n
-
-applyTechnique deck (INCREMENT n) = go deck 0 (V.replicate len undefined) where
-    len = length deck
-    go []     _ vec = V.toList vec
-    go (x:xs) r vec = go xs ((r + n) `mod` len) (vec // [(r, x)])
-
-
-parseInput :: String -> IO [Technique]
+parseInput :: String -> IO Shuffle
 parseInput raw = do
-    case parse inputFile "" raw of
-        Left e  -> error $ errorBundlePretty e
-        Right x -> return x
+    case parse (many (newStack <|> cutCards <|> increment)) "" raw of
+        Left  e -> error $ errorBundlePretty e
+        Right x -> return $ foldl' (flip (.)) id x
 
 
-inputFile :: Parser [Technique]
-inputFile = many (newStack <|> cutCards <|> increment)
-
-newStack :: Parser Technique
+newStack :: Parser Shuffle
 newStack = do
     string "deal into new stack"
     newline
-    return NEWSTACK
+    return reverse
 
-cutCards :: Parser Technique
+cutCards :: Parser Shuffle
 cutCards = do
     string "cut "
     n <- read <$> many (oneOf "-0123456789")
     newline
-    return $ CUTCARDS n
+    return (\deck -> uncurry (++) $ swap $ splitAt (n `mod` (length deck)) deck)
 
-increment :: Parser Technique
+increment :: Parser Shuffle
 increment = do
     string "deal with increment "
     n <- read <$> many (oneOf "0123456789")
     newline
-    return $ INCREMENT n
+    return (\deck -> map snd $ sort $ map (\(a, b) -> ((a * n) `mod` (length deck), b)) (zip [0..] deck))
