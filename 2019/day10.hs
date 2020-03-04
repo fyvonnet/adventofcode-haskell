c93eb16b8211c4fef6f162562532181bbf5cbfa4
Franck Yvonnet <franck@yvonnet.fr>
2020-03-04 14:53:49 +0100

    code refactoring

--- 2019/day10.hs
+++ 2019/day10.hs
@@ -1,30 +1,52 @@
 import           AOC.Common
 import           AOC.Coord
-import           Data.List (sortBy)
+import           Data.List (sortBy, maximumBy)
 import           Data.Map (Map, (!))
 import qualified Data.Map as M
 
+
 main :: IO ()
 main = do
-    --raw <- readFile "inputs/day10-test"
     raw <- readFile "inputs/day10"
     let asteroids = map fst $ filter (\x -> snd x == '#') $ getTextMap raw
 
-    let (count, coord, m) = maximum $ map (func asteroids) asteroids
-    print count
+    let (coord, astByAngle) = maximumBy compareLength $ map (func asteroids) asteroids
+    print $ M.size astByAngle
 
-    let m' = M.map (sortBy (\a b -> compare (distance coord a) (distance coord b))) m
-    print $ (\(Coord x y) -> x * 100 + y) $ (!! 199) $ map (\k -> head (m ! k)) $ M.keys m
+    let astByAngle' = map (sortBy (compareDistance coord)) $ M.elems astByAngle
+    print $ (\(Coord x y) -> x * 100 + y) $ (destroyAsteroids astByAngle') !! 199
 
 
-    
-func lst c = (\x -> (M.size x, c, x)) $ foldl (insertCoord c) M.empty $ filter (/= c) lst
+destroyAsteroids :: [[Coord]] -> [Coord]
+destroyAsteroids lst = snd $ until (null . fst) laserRotation (lst, [])
+
+
+laserRotation :: ([[Coord]], [Coord]) -> ([[Coord]], [Coord])
+laserRotation (lst, dest) = (lst', dest') where
+    dest' = dest ++ map head lst
+    lst'  = filter (not . null) $ map tail lst
+
+
+func :: [Coord] -> Coord -> (Coord, Map Float [Coord])
+func lst c = (c, foldl (insertCoord c) M.empty $ filter (/= c) lst)
+
 
 insertCoord :: Coord -> Map Float [Coord] -> Coord -> Map Float [Coord]
 insertCoord o m c = M.insert a (c : (M.findWithDefault [] a m)) m
     where a = angle o c
 
+
 angle :: Coord -> Coord -> Float
 angle (Coord p0x p0y) (Coord p1x p1y) = negate $ atan2 (fromIntegral (p1x - p0x)) (fromIntegral (p1y - p0y))
 
+
+compareLength :: (Coord, Map Float [Coord]) -> (Coord, Map Float [Coord]) -> Ordering
+compareLength (_, a) (_, b) = compare (M.size a) (M.size b)
+
+
+compareDistance :: Coord -> Coord -> Coord -> Ordering
+compareDistance orig a b = compare (distance orig a) (distance orig b)
+    
+
+distance :: Coord -> Coord -> Int
 distance (Coord p0x p0y) (Coord p1x p1y) = max (abs (p0x - p1x)) (abs (p0y - p1y))
