4d4fe19b3743830506dc5c049ac787aaf29ebc14
Franck Yvonnet <franck@yvonnet.fr>
2020-03-04 10:37:15 +0100

    new movement functions

--- modules/AOC/Coord.hs
+++ modules/AOC/Coord.hs
@@ -2,6 +2,9 @@ module AOC.Coord where
 
 import Data.Array
 
+data AbsDirection = NORTH | EAST | SOUTH | WEST deriving (Show, Eq, Enum, Ord, Bounded)
+data RelDirection = FRONT | RIGHT | BACK | LEFT deriving (Show, Eq, Enum, Ord, Bounded)
+
 data Coord = Coord {_x :: Int, _y :: Int } deriving Ix
 --data Coord = Coord {_x :: Int, _y :: Int } deriving (Show, Ix)
 
@@ -22,3 +25,50 @@ addCoord (Coord ax ay) (Coord bx by) = (Coord (ax + bx) (ay + by))
 
 taxicab :: Coord -> Coord -> Int
 taxicab (Coord ax ay) (Coord bx by) = (abs (ax - bx)) + (abs (ay - by))
+
+
+-- to be removed
+nextCoord :: Coord -> AbsDirection -> Coord
+nextCoord (Coord x y) WEST  = (Coord (x - 1) y)
+nextCoord (Coord x y) EAST  = (Coord (x + 1) y)
+nextCoord (Coord x y) SOUTH = (Coord x (y + 1))
+nextCoord (Coord x y) NORTH = (Coord x (y - 1))
+
+absNeighbour ::  AbsDirection -> Coord -> Coord 
+absNeighbour WEST  (Coord x y) = (Coord (x - 1) y)
+absNeighbour EAST  (Coord x y) = (Coord (x + 1) y)
+absNeighbour SOUTH (Coord x y) = (Coord x (y + 1))
+absNeighbour NORTH (Coord x y) = (Coord x (y - 1))
+
+relNeighbour :: AbsDirection -> RelDirection -> Coord -> Coord
+relNeighbour a r c = absNeighbour (turn r a) c
+
+
+allAbsDirections :: [AbsDirection]
+allAbsDirections = [minBound..]
+
+
+-- to be removed
+allNextCoords :: Coord -> [Coord] 
+allNextCoords c = map (nextCoord c) allAbsDirections
+
+
+allNeighbours :: Coord -> [Coord] 
+allNeighbours c = map (\d -> absNeighbour d c) allAbsDirections
+
+
+turn :: RelDirection -> AbsDirection -> AbsDirection
+
+turn FRONT ad   = ad
+
+turn BACK WEST  = EAST
+turn BACK EAST  = WEST
+turn BACK SOUTH = NORTH
+turn BACK NORTH = SOUTH
+
+turn RIGHT WEST = NORTH
+turn RIGHT ad   = succ ad
+
+turn LEFT NORTH = WEST
+turn LEFT ad    = pred ad
+
