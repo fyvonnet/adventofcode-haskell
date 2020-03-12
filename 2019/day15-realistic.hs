52f89e64d81e51a76e17cc9cdc8542a28e7b9894
Franck Yvonnet <franck@yvonnet.fr>
2020-03-12 15:13:46 +0100

    refactored the explore function

--- 2019/day15-realistic.hs
+++ 2019/day15-realistic.hs
@@ -2,24 +2,20 @@
 
 
 import           AOC.Coord
-import           Control.Lens
-import           Data.Map (Map)
+import           Control.Lens ((&), (%~), (+~), (-~), (.~), (^.), makeLenses)
 import           Data.Set (Set)
 import           IntCode
-import qualified Data.Map as Map
 import qualified Data.Set as Set
 
 
-data Square = WALL | PASSAGE [AbsDirection] deriving (Show, Eq)
-type Area   = Map Coord Square
-
+data Move   = FW AbsDirection | BK AbsDirection 
 data RobotState = RobotState
-    { _coord  :: Coord
-    , _trail  :: [AbsDirection]
-    , _ics    :: ICState
-    , _nsteps :: Int
-    , _oxy    :: Coord
-    , _area   :: Area
+    { _coord   :: Coord
+    , _ics     :: ICState
+    , _steps   :: Int
+    , _oxy_stp :: Int
+    , _oxy_crd :: Coord
+    , _walls   :: Set Coord
     }
 
 makeLenses ''RobotState
@@ -29,57 +25,47 @@ main :: IO ()
 main = do
     ics <- loadCode "inputs/day15"
 
-    let rs = explore RobotState { 
-        _coord  = (Coord 0 0) ,
-        _trail  = [] ,
-        _ics    = ics ,
-        _nsteps = undefined ,
-        _oxy    = undefined ,
-        _area   = Map.singleton (Coord 0 0) (PASSAGE allAbsDirections) }
+    let rs = explore (map FW allAbsDirections) RobotState { 
+        _coord   = (Coord 0 0) ,
+        _ics     = ics         ,
+        _steps   = 1           ,
+        _oxy_stp = undefined   ,
+        _oxy_crd = undefined   ,
+        _walls   = Set.empty   }
 
-    print $ rs^.nsteps
-    print $ expand (Set.fromList $ Map.keys $ Map.filter (== WALL) $ rs^.area) [(1, rs^.oxy)] 0
+    print (rs^.oxy_stp)
+    print $ fill (rs^.walls) [(1, rs^.oxy_crd)] 0
 
 
-expand :: Set Coord -> [(Int, Coord)] -> Int -> Int
-expand _       []    time = time
-expand blocked ((t, coord):qs) time = expand blocked' queue t
+explore :: [Move] -> RobotState -> RobotState
+
+explore []             rs = rs
+
+explore ((BK dir):mvs) rs = explore mvs $ rs
+    & steps -~ 1
+    & coord %~ absNeighbour dir
+    & ics   %~ snd . runIntCode (dir2cmd dir)
+
+explore ((FW dir):mvs) rs = case output of
+    0 -> explore mvs  rs  & walls %~ Set.insert (absNeighbour dir (rs^.coord))
+    1 -> explore mvs' rs'
+    2 -> explore mvs' rs' & (oxy_stp .~ rs^.steps) & (oxy_crd .~ rs^.coord)
+    where
+        ([output], ics') = runIntCode (dir2cmd dir) (rs^.ics)
+        turnBack = turn BACK dir
+        mvs' = [FW d | d <- allAbsDirections, d /= turnBack] ++ [BK turnBack] ++ mvs
+        rs'  = rs
+            & steps +~ 1
+            & coord %~ absNeighbour dir
+            & ics   .~ ics'
+
+
+fill :: Set Coord -> [(Int, Coord)] -> Int -> Int
+fill _       []              time = time
+fill blocked ((t, coord):qs) time = fill blocked' queue t
     where
         blocked' = Set.insert coord blocked
         queue    = qs ++ [(t + 1, c) | c <- allNextCoords coord, Set.notMember c blocked]
-
-
-explore :: RobotState -> RobotState
-explore rs = 
-
-    case (av, rs^.trail, output) of
-
-        ([], [], _) -> rs
-
-        ([],  _, _) -> explore $ rs
-            & coord %~ absNeighbour turnBack
-            & ics   %~ snd . runIntCode (dir2cmd turnBack)
-            & trail %~ tail
-
-        (_, _, [0]) -> explore $ rs'
-            & area %~ Map.insert (absNeighbour (head av) (rs^.coord)) WALL
-
-        (_, _, [1]) -> explore   rs_moved
-        
-        (_, _, [2]) -> explore $ rs_moved
-            & nsteps .~ (length $ rs^.trail) + 1
-            & oxy    .~ rs^.coord
-
-    where
-        (output, ics') = runIntCode (dir2cmd $ head av) (rs^.ics)
-        (PASSAGE av)   = Map.findWithDefault defAvail (rs^.coord) (rs^.area)
-        defAvail = PASSAGE $ filter (\d -> d /= turnBack) allAbsDirections
-        turnBack = turn BACK $ head (rs^.trail)
-        rs'      = rs & area %~ Map.insert (rs^.coord) (PASSAGE $ tail av)
-        rs_moved = rs' 
-            & ics   .~ ics'
-            & coord %~ absNeighbour (head av)
-            & trail %~ (:) (head av)
 
 
 dir2cmd :: AbsDirection -> [Int]
